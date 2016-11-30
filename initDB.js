4c9f279e08ef7f62b81345e67df33a52ec9c282b
= <=>
2016-11-29 23:40:48 -0200

    arrumei o end do banco de dados

--- initDB.js
+++ initDB.js
@@ -1,3 +1,3 @@
 var pgp = require("pg-promise")();
-db = pgp("postgres://postgres:whenyougrowup@127.0.0.1:5432/embarcados");
+db = pgp("postgres://dxddwwmduowraq:c4pBIaItKeoodw5tKhtNG360Fa@ec2-54-225-246-33.compute-1.amazonaws.com:5432/dakgsb02m90ic");
 module.exports = db;
